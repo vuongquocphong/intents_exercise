34e833a9841839755053ecc2e8a486fe5ab042a5
produy <nguyentrinhduy2003@gmail.com>
2023-06-18 21:52:11 +0700

    initial push

--- app/src/main/java/com/example/intents_exercise/MainActivity.java
+++ app/src/main/java/com/example/intents_exercise/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.intents_exercise;
 
+import static java.net.Proxy.Type.HTTP;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button getMail;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getMail = findViewById(R.id.ButtonClick);
+        et = findViewById(R.id.EnterEmail);
+        getMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] s = new String[1];
+                s[0] = et.getText().toString();
+                composeEmail(s, "hello from the other side");
+            }
+        });
+    }
+    public void composeEmail(String[] addresses, String subject) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // Only email apps handle this.
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        startActivity(intent);
     }
 }
