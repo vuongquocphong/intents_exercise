16df99f63b51eb2228b7f898be06010f6740c9d6
produy <nguyentrinhduy2003@gmail.com>
2023-06-18 21:22:50 +0700

    initial push

--- app/src/main/java/com/example/intents_exercise/MainActivity.java
+++ app/src/main/java/com/example/intents_exercise/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.intents_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button getWeb;
+    EditText et_data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWeb = findViewById(R.id.GetWeb);
+        et_data = findViewById(R.id.LinkEnter);
+        getWeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage(et_data.getText().toString());
+            }
+        });
     }
+    public void openWebPage(String url) {
+        if (!url.startsWith("http://") || !url.startsWith("https://")){
+            url = "http://" + url;
+        }
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+
 }
