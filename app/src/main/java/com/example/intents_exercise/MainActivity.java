b070f1c5db3f05e70969a458b4370067a0438f71
produy <nguyentrinhduy2003@gmail.com>
2023-06-18 22:03:33 +0700

    intial push

--- app/src/main/java/com/example/intents_exercise/MainActivity.java
+++ app/src/main/java/com/example/intents_exercise/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.intents_exercise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnMessage;
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMessage = findViewById(R.id.btn_message);
+        et = findViewById(R.id.Message);
+        btnMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                composeMmsMessage(et.getText().toString(), "hello world!");
+            }
+        });
+    }
+    public void composeMmsMessage(String phoneNumber, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("smsto:" + phoneNumber));  // Only SMS apps respond to this.
+        intent.putExtra("sms_body", message);
+        startActivity(intent);
     }
 }
